33e2903d35fa1ed1410963a889dabba2440519f0
zeyadg37 <zeyad.gasser@gmail.com>
2023-03-01 13:02:09 +0100

    setup gradle plugin publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,44 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.44" apply false
     id("java-gradle-plugin")
     id("maven-publish")
-    id("com.gradle.plugin-publish") version "0.16.0"
+    id("com.gradle.plugin-publish") version "1.1.0"
+    `ivy-publish`
 }
 apply<GitHooksPlugin>()
 
 pluginBundle {
-    website = "<substitute your project website>"
-    vcsUrl = "<uri to project source repository>"
-    tags = listOf("tags", "for", "your", "plugins")
+    website = "https://github.com/Zeyad-37/AndroidDevOps"
+    vcsUrl = "https://github.com/Zeyad-37/AndroidDevOps.git"
+    tags = listOf("devOps", "android", "gradle", "github-actions", "gradle-plugins", "kotlin")
+}
+
+group = "io.github.zeyadgasser"
+version = "1.0"
+
+gradlePlugin {
+    plugins {
+        create("androidModulePlugin") {
+            id = "com.zeyadgasser.gradle.plugin.android-module-plugin"
+            implementationClass = "com.zeyadgasser.plugins.AndroidModulePlugin"
+            displayName = "AndroidModulePlugin"
+            description = "Plugin for setting up android modules"
+        }
+    }
+    plugins {
+        create("gitHooksPlugin") {
+            id = "com.zeyadgasser.gradle.plugin.git-hooks-plugin"
+            implementationClass = "com.zeyadgasser.plugins.GitHooksPlugin"
+            displayName = "GitHooksPlugin"
+            description = "Plugin install git hooks from config file"
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "localPluginRepository"
+            url = uri("../local-plugin-repository")
+        }
+    }
 }
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -24,26 +24,9 @@ repositories {
 
 dependencies {
     api("com.android.tools.build:gradle:7.4.1")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
     api(plugin("io.gitlab.arturbosch.detekt", version = "1.22.0"))
     api(plugin("org.jetbrains.kotlinx.kover", version = "0.6.1"))
-    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
-}
-
-gradlePlugin {
-    plugins {
-        create("androidModulePlugin") {
-            id = "com.zeyadgasser.gradle.android-module-plugin"
-            implementationClass = "com.zeyadgasser.plugins.AndroidModulePlugin"
-            displayName = "AndroidModulePlugin"
-            description = "Plugin for setting up android modules"
-        }
-        create("gitHooksPlugin") {
-            id = "com.zeyadgasser.gradle.git-hooks-plugin"
-            implementationClass = "com.zeyadgasser.plugins.GitHooksPlugin"
-            displayName = "GitHooksPlugin"
-            description = "Plugin install git hooks from config file"
-        }
-    }
 }
 
 fun DependencyHandler.plugin(id: String, version: String) =
